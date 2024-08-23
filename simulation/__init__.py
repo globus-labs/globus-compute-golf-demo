3a467f285b236c97f8a0907133ac7685b4e418d8
sa7hvick <163932077+sa7hvick@users.noreply.github.com>
2024-08-22 23:59:58 -0400

    Update __init__.py

--- simulation/__init__.py
+++ simulation/__init__.py
@@ -1,2 +1,2 @@
 # __init__.py
-from .simulate import generate_noisemap, generate_vertices, run_simulation, generate_heatmap
+from .simulate import generate_noisemap, generate_vertices, run_simulation, generate_heatmap, generate_3d_heatmap
